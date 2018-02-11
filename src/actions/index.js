fa01ef409cd8cc6af4f3f4be8a0ce88231a3f11e
damjandado <damjanspace@gmail.com>
2018-02-11 21:18:46 +0100

    initial commit

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API_KEY = 'e41698d35db6fe68499bf8fe215c8f9b';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},rs`;
+  const request = axios.get(url);
+
+  console.log('Request:', request);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
