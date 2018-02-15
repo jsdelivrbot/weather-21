ffb850cfb152b8ff4a3162fe1b9119f42ece9287
damjandado <damjanspace@gmail.com>
2018-02-16 00:16:21 +0100

    add logo

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,43 +1,76 @@
+// import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { fetchWeather } from '../actions/index'
- 
+import { fetchWeather } from '../actions/index';
+import countries from '../components/countries';
+// var countries = require('country-list')();
+// const countries = cl();
+
 class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { term: '' };
+    this.state = { term: '', country: 'AF' };
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.onSelectChange = this.onSelectChange.bind(this);
     this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event) {
-    this.setState({ term: event.target.value })
+    this.setState({ term: event.target.value });
+  }
+
+  onSelectChange(event) {
+    console.log(event.target);
+    this.setState({ country: event.target.value });
   }
 
   onFormSubmit(event) {
     event.preventDefault();
 
-    this.props.fetchWeather(this.state.term);
+    this.props.fetchWeather(this.state.term, this.state.country);
     this.setState({ term: '' });
   }
 
+  renderCountryList() {
+    const countryNames = _.map(countries, 'Name');
+    return _.map(countryNames, (c, i) => <option key={i}>{c}</option>);
+  }
+
   render() {
+    console.log(countries);
     return (
-      <form onSubmit={this.onFormSubmit} className="input-group">
-        <input
-          placeholder="Get a five-day forecast for your cities"
-          className="form-control"
-          value={this.state.term}
-          onChange={this.onInputChange}
-        />
-        <span className="input-group-btn">
-          <button type="submit" className="btn btn-secondary">
-            Тражи
-          </button>
-        </span>
+      <form onSubmit={this.onFormSubmit}>
+        <div className="form-row">
+          <div className="form-group col-md-6">
+            <input
+              placeholder="Get a five-day forecast for your cities"
+              className="form-control"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+          <div className="form-group col-md-2">
+            <label htmlFor="inputState" className="sr-only">
+              State
+            </label>
+            <select
+              id="inputState"
+              className="form-control"
+              value={this.state.country}
+              onChange={this.onSelectChange}
+            >
+              {this.renderCountryList()}
+            </select>
+          </div>
+          <div className="form-group col-md-4">
+            <button type="submit" className="btn btn-secondary">
+              Search
+            </button>
+          </div>
+        </div>
       </form>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,32 @@
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
+  entry: ["./src/index.js"],
   output: {
     path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
+    publicPath: "/",
+    filename: "bundle.js"
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
+    rules: [
+      {
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["react", "env", "stage-3"]
+        }
+      },
+      // { test: /\.(png|jpg|gif)$/, loader: "url-loader?limit=8192" },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: "file-loader",
+        options: { name: "[path][name].[hash].[ext]" }
       }
-    }]
+    ]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: [".js", ".jsx"]
   },
   devServer: {
     historyApiFallback: true,
-    contentBase: './'
+    contentBase: "./"
   }
 };
