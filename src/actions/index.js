b39179e3f5be20939e811149dd48aedcab96048b
damjandado <damjanspace@gmail.com>
2018-02-16 15:49:10 +0100

    first heroku

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = 'e41698d35db6fe68499bf8fe215c8f9b';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
