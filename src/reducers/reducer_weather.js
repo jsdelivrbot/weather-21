5c1c9a0cf8574d34307a5c17c67652f7142e9fc3
dacheaux <damjanspace@gmail.com>
2018-09-04 23:25:54 +0200

    september 2018 update

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -2,9 +2,10 @@ import { FETCH_WEATHER } from '../actions';
 
 export default function(state = [], action) {
   console.log('Action received', action);
-  switch(action.type) {
+  switch (action.type) {
     case FETCH_WEATHER:
-      return [ action.payload.data, ...state ];
+      if (action.error) return state;
+      return [action.payload.data, ...state];
   }
   return state;
 }
