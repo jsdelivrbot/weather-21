2070323a068a3d45f8eeaaae54bb7918b6cc6b49
dacheaux <damjanspace@gmail.com>
2018-09-05 15:59:37 +0200

    september 2018 update

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,45 @@
 const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebPackPlugin({
+  template: './index.html',
+  filename: 'index.html'
+});
 
 module.exports = {
   module: {
     rules: [
       {
         exclude: /node_modules/,
-        loader: "babel-loader"
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true }
+          }
+        ]
       },
       // { test: /\.(png|jpg|gif)$/, loader: "url-loader?limit=8192" },
       {
         test: /\.(png|jpg|gif)$/,
-        loader: "file-loader",
-        options: { name: "[path][name].[hash].[ext]" }
+        loader: 'file-loader',
+        options: { name: '[path][name].[hash].[ext]' }
       }
     ]
   },
   resolve: {
-    extensions: [".js", ".jsx"]
+    extensions: ['.js', '.jsx']
   },
   devServer: {
-    contentBase: path.resolve(__dirname, "dist"),
+    contentBase: './',
     historyApiFallback: true,
     inline: true,
     open: true,
     compress: true,
     hot: true
-  }
+  },
+  plugins: [HtmlWebpackPluginConfig]
 };
